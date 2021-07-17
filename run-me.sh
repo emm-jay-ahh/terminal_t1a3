c4504794e6b3ee9c3c48ca59ac307e918b52fcf8
emm-jay-ahh <mattrooney.wrk@gmail.com>
2021-07-17 19:01:00 +1000

    added check for python3

--- run-me.sh
+++ run-me.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+if ! [[ -x "$(command -v python3)" ]]
+then
+	echo "ERROR:
+
+    Python3 is not installed
+    Please goto https://www.python.org/downloads/
+    DOWNLOAD and INSTALL Python3" >&2
+
+	exit 1
+fi
 
 sudo python3 net-whack.py $1
